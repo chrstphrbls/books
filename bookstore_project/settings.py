ea3e9c81231336080bff1effe4532e11d18d07e3
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-27 17:09:44 +0800

    models migrate

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
