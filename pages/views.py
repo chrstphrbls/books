9411146acdf63152ef5141531e2e17a91a542400
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-29 14:39:24 +0800

    modified views

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# pages/views.py
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomepageView(TemplateView):
+    template_name = 'home.html'
+
+    
+
