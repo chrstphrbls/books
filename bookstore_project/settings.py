007d39527dc302c7c43aa37f08ea8b77b7aaf7b4
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-29 14:14:29 +0800

    added templates

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')], #set templates location - html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
