35e88cb7d42cefd97534e0453c434d76d543303d
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-07-29 12:15:31 +0800

    pages
    added the pages app to the settings

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # local
-    'users.apps.UsersConfig',
+    'users.apps.UsersConfig', #users app
+    'pages.apps.PagesConfig', #pages app
 ]
 
 MIDDLEWARE = [
