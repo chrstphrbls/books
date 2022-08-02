8fac6f66936db7e6a08c792ee9000eaf881685db
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-08-02 13:36:40 +0800

    fixed the bootstrap

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 # pages/urls.py
 from django.urls import path
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
 urlpatterns=[ 
+    path('about/', AboutPageView.as_view(), name='about'),
     path('',HomePageView.as_view(),name='home'),
 ]
 
