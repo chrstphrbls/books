af5a43dc4d04e4b8c7e2c23f191c95894fa88a2c
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-08-03 13:47:35 +0800

    secret key
    added feature of secret key

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(xx))r=+35dxpw!ybud-o!#m642=k7qd%hr^+pvoxd^2@wxl5('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
