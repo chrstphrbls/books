bc28784cb754317940f9515326a401330ec4ced9
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-08-04 15:41:20 +0800

    reviews
    added reviews model

--- books/models.py
+++ books/models.py
@@ -1,9 +1,15 @@
 # books/models.py
+# import uuid
 from audioop import reverse
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 class Book(models.Model):
+    # id = models.UUIDField(
+    #     primary_key=True,
+    #     default=uuid.uuid4,
+    #     editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -13,3 +19,20 @@ class Book(models.Model):
 
     def get_absolute_url(self): # new
         return reverse('book_detail', args=[str(self.id)])
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        Book,
+        on_delete = models.CASCADE,
+        related_name = 'reviews',
+    )
+    review = models.CharField(max_length = 255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
+
+        
