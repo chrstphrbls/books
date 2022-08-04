f39368a76a69916975ed406fc26d7e54153d1ac8
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-08-04 15:32:47 +0800

    edited reverse

--- books/models.py
+++ books/models.py
@@ -1,22 +1,22 @@
 # books/models.py
 import uuid
+from audioop import reverse
 from django.db import models
 from django.urls import reverse
 
 class Book(models.Model):
     id = models.UUIDField(
-        primary_key = True,
-        default = uuid.uuid4,
-        editable = False,
-    )
+    primary_key=True,
+    default=uuid.uuid4,
+    editable=False)
+
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length = 200)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.title 
 
-    def get_absolute_url(self):
-        return reverse('book_detail', args=[str(self.id)])
-
-
+    def get_absolute_url(self): # new
+        return reverse('book_detail', kwargs={'pk': str(self.pk)})
+ 
