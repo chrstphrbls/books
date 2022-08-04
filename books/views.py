0bab573ca32d4437fd8f2ec0eb3801652a2cf890
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-08-04 11:03:01 +0800

    books page
    added view

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 # books/views.py
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView 
 
 from .models import Book
 
@@ -7,3 +7,7 @@ class BookListView(ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
