ad95b83ae2d56ab492334f298c212f8567eee71b
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-08-08 14:36:34 +0800

    added allowed host

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokkuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
