d7a8029dc5f0aa9fd9afb3ff45def1c1d172810f
chrstphrbls <109645414+chrstphrbls@users.noreply.github.com>
2022-08-08 08:34:17 +0800

    added Permissions
    if user is not logged it. they will be redirected to the log in page instead of the page they are currently accessing

--- books/views.py
+++ books/views.py
@@ -1,13 +1,16 @@
 # books/views.py
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView 
 
 from .models import Book
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+    login_url = 'account_login'
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+    login_url = 'account_login'
